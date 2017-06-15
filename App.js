dafe273416badcd00978edc762aafb4a17ccb937
Chandika Guruge <gurugecl@gmail.com>
2017-06-14 22:45:22 -0500

    Attempt to create google view

--- App.js
+++ App.js
@@ -4,18 +4,39 @@ import { StyleSheet, Text, View, WebView } from 'react-native';
 export default class App extends React.Component {
     render() {
         return (
-            <WebView source={{uri: 'http://68.184.199.16:8081/vr/'}} />
+            <WebView style={styles.container}>
+                <WebView source={{uri: 'http://192.168.2.13:8081/vr/'}} style={{margin: 20}} />
+                <WebView source={{uri: 'http://192.168.2.13:8081/vr/'}} style={{margin: 20}} />
+            </WebView>
         );
     }
 }
 
-// 192.168.60.108
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        backgroundColor: 'black',
+        z-index: -1
+    },
+});
 
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         backgroundColor: '#fff',
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//     },
-// });
+
+// export default class JustifyContent extends Component {
+//     render() {
+//         return (
+//             <View style={{
+//                 flex: 1,
+//                 flexDirection: 'column',
+//                 justifyContent: 'space-around',
+//                 alignItems: 'center',
+//                 backgroundColor: 'black'
+//             }}>
+//                 <View style={{width: 195, height: 195, backgroundColor: 'steelblue'}} />
+//                 <View style={{width: 195, height: 195, backgroundColor: 'steelblue'}} />
+//             </View>
+//         );
+//     }
+// };
